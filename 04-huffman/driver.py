32a6ce6fb1a901b21bcf0ab94b89e63985278003
Olga Emelyanova <volha.emelianova@gmail.com>
2020-10-23 00:36:48 +0300

    Add huffman compression

--- 04-huffman/driver.py
+++ 04-huffman/driver.py
@@ -2,11 +2,14 @@ from huffman import Huffman
 
 import argparse
 import re
+import math
+import itertools
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--file', help='File to compress')
 parser.add_argument('--out1', help='Where to save file compressed based on frequency of letters in language in general')
 parser.add_argument('--out2', help='Where to save file compressed based on frequencies of letters in that particular file')
+parser.add_argument('--out3', help='Where to save file compressed based on frequencies of pairs of letters in that particular file')
 args = parser.parse_args()
 
 regex = re.compile('[^ абвгдежзийклмнопрстуфхцчшщьыюя]')
@@ -48,12 +51,26 @@ huffman_from_text.encode(contents, args.out2)
 print('Source entropy based on letter probabilities is {:1.2f}'.format(huffman_from_text.entropy()))
 print('Average codeword length is {:1.2f}'.format(huffman_from_text.average_code_len()))
 
-# Ask Lisa about pairs: is it simply about 2 consecutive symbols?
-# относительная энтропия = cross entropy?
+prob_dict = {sym:freq for freq,sym in prob}
+relative_entropy = 0
+for f_x,x in text_prob:
+    g_x = prob_dict[x]
+    relative_entropy += f_x * math.log(f_x / g_x)
+print('Relative entropy is {:1.2f}'.format(relative_entropy))
 
+freq = {}
+as_pairs = [contents[i*2:(i+1)*2] for i in range(len(contents) // 2)]
+for pair in as_pairs:
+    if pair not in freq:
+        freq[pair] = 1
+    else:
+        freq[pair] += 1
 
+pair_prob = []
+for pair,freq in freq.items():
+    pair_prob.append((freq/len(as_pairs), pair))
 
-
-
-
-
+huffman_pairs = Huffman(pair_prob)
+huffman_pairs.encode(as_pairs, args.out3)
+print('Source entropy based on pair probabilities is {:1.2f}'.format(huffman_pairs.entropy()))
+print('Average codeword length is {:1.2f}'.format(huffman_pairs.average_code_len()))
